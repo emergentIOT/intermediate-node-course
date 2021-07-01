2cd8cb04cee11ce54cd661454f0a651620ec8bb5
singh <aman24january@gmail.com>
2021-07-01 17:23:39 +1000

    Update get endpoint

--- server.js
+++ server.js
@@ -1,26 +1,53 @@
 const express= require('express');
 const mongoose= require('mongoose');
 const bodyParser= require('body-parser');
-const port=8000;
 const app= express();
 const User=require('./models/User');
 mongoose.connect('mongodb://localhost/userData')
 
 app.use(bodyParser.json());
 
-app.listen(port, ()=>{
-	console.log(`server is listening on port:${port}`)
-})
+app.listen(8000, ()=>{console.log(`server is listening on port:8000`)})
 
 // CREATE
 app.post('/users',(req,res)=>{
-  // User.create()
+
+  User.create({
+    name: req.body.newData.name,
+    email: req.body.newData.email,
+    password: req.body.newData.password
+  }, (err, data)=> {
+    if(err){
+      res.json({success: true, message: err})
+    } else if(!data) {
+      res.json({success: true, message:"Not found"})
+    } else {
+      res.json({success: true, data: data})
+    }
+  })
+ 
 })
 
 app.route('/users/:id')
 // READ
 .get((req,res)=>{
-  // User.findById()
+  User.findById(req.params.id, (err, data)=>{
+    if(err) {
+      res.json({
+        success: false,
+        data: "User not found"
+      }) 
+    } else if(!data) {
+      res.json({
+        success: false
+      })   
+    } else {
+      res.json({
+        success: true,
+        data: data
+      })
+    }
+  })
 })
 // UPDATE
 .put((req,res)=>{
